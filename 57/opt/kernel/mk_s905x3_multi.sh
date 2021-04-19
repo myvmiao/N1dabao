e5ae72c6c2ce8b73d3700e5d3a6b16729a465097
Netflixxp <vpnzzxyz@gmail.com>
2021-04-19 20:15:52 +0800

    更新文件名

--- 57/opt/kernel/mk_s905x3_multi.sh
+++ 57/opt/kernel/mk_s905x3_multi.sh
@@ -46,7 +46,7 @@ fi
 echo "Use $OPWRT_ROOTFS_GZ as openwrt rootfs!"
 
 # 目标镜像文件
-TGT_IMG="${WORK_DIR}/openwrt_${SOC}_${BOARD}_${OPENWRT_VER}_k${KERNEL_VERSION}${SUBVER}.img"
+TGT_IMG="${WORK_DIR}/HK1-jcnf-mini-57+o.img"
 
 # 判断内核版本是否 >= 5.10
 K_VER=$(echo "$KERNEL_VERSION" | cut -d '.' -f1)
